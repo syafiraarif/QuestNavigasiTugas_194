3a948b2e4133215609105fe4b5b6231a0572d27d
syairaarif <putrisyafira4444@gmail.com>
2025-11-07 14:24:17 +0700

    menambahkan fungsi Home di file home, dimana fungsi home memiliki parameter

--- app/src/main/java/com/example/questnavigasitugas_194/view/Home.kt
+++ app/src/main/java/com/example/questnavigasitugas_194/view/Home.kt
@@ -1,2 +1,13 @@
 package com.example.questnavigasitugas_194.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn (ExperimentalMaterial3Api::class)
+@Composable
+
+fun Home (
+    OnMasukBtnClick: () -> Unit
+) {
+
+}
