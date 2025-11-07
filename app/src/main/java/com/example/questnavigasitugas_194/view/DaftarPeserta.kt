2b4c78c10fa8dee7dee509eeab3a5da701f52c49
syairaarif <putrisyafira4444@gmail.com>
2025-11-07 14:52:02 +0700

    membuat fungsi ini itu layout utamanya adalah column

--- app/src/main/java/com/example/questnavigasitugas_194/view/DaftarPeserta.kt
+++ app/src/main/java/com/example/questnavigasitugas_194/view/DaftarPeserta.kt
@@ -1,7 +1,15 @@
 package com.example.questnavigasitugas_194.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.unit.dp
 
 @OptIn (ExperimentalMaterial3Api::class)
 @Composable
@@ -10,5 +18,12 @@ fun ListPeserta(
     OnBerandaClick: () -> Unit,
     OnFormulirClick: () -> Unit
 ) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {  }
 
 }
