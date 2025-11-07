af1520a913edf3d2af9a11b7de733c0beee56370
syairaarif <putrisyafira4444@gmail.com>
2025-11-07 15:15:40 +0700

    menambahkan komponen spacer untuk memberi jarak

--- app/src/main/java/com/example/questnavigasitugas_194/view/DaftarPeserta.kt
+++ app/src/main/java/com/example/questnavigasitugas_194/view/DaftarPeserta.kt
@@ -3,8 +3,10 @@ package com.example.questnavigasitugas_194.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -85,6 +87,10 @@ fun ListPeserta(
                             )
                         }
                     }
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+
                 }
             }
         }
