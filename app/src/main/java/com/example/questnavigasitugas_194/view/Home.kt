4f5be4809398da1d531496406d8e876d2c882dea
syairaarif <putrisyafira4444@gmail.com>
2025-11-07 14:32:42 +0700

    menambahkan komponen text yang akan menampilkan tulisan Selamat Datang

--- app/src/main/java/com/example/questnavigasitugas_194/view/Home.kt
+++ app/src/main/java/com/example/questnavigasitugas_194/view/Home.kt
@@ -5,11 +5,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questnavigasitugas_194.R
 
 @OptIn (ExperimentalMaterial3Api::class)
 @Composable
@@ -23,5 +29,14 @@ fun Home (
             .padding (horizontal = 24.dp, vertical = 32.dp),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {}
+    ) {
+        Text(
+            text = stringResource(id = R.string.judul_welcome),
+            style = MaterialTheme.typography.titleLarge.copy (
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            ),
+            textAlign = TextAlign.Center
+        )
+    }
 }
