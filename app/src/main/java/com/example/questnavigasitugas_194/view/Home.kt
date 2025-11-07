6b9a3915c01d9bda870b54a8e091e5e4699739c0
syairaarif <putrisyafira4444@gmail.com>
2025-11-07 14:28:34 +0700

    membuat layout column di halaman utama

--- app/src/main/java/com/example/questnavigasitugas_194/view/Home.kt
+++ app/src/main/java/com/example/questnavigasitugas_194/view/Home.kt
@@ -1,7 +1,15 @@
 package com.example.questnavigasitugas_194.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.unit.dp
 
 @OptIn (ExperimentalMaterial3Api::class)
 @Composable
@@ -9,5 +17,11 @@ import androidx.compose.runtime.Composable
 fun Home (
     OnMasukBtnClick: () -> Unit
 ) {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding (horizontal = 24.dp, vertical = 32.dp),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {}
 }
