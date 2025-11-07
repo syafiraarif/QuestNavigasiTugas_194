98df7add0ffc817c27429beac2d8b72ff93321c3
syairaarif <putrisyafira4444@gmail.com>
2025-11-07 14:49:39 +0700

    mmbuat fungsi ListPserta

---
+++ app/src/main/java/com/example/questnavigasitugas_194/view/DaftarPeserta.kt
@@ -0,0 +1,14 @@
+package com.example.questnavigasitugas_194.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn (ExperimentalMaterial3Api::class)
+@Composable
+
+fun ListPeserta(
+    OnBerandaClick: () -> Unit,
+    OnFormulirClick: () -> Unit
+) {
+
+}
