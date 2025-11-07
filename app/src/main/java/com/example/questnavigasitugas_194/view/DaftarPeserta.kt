6dc8be7a370fb370e0b145dc25bbbc6066852cfd
syairaarif <putrisyafira4444@gmail.com>
2025-11-07 14:55:55 +0700

    menambahkan komponen text sebagai judul daftar peserta

--- app/src/main/java/com/example/questnavigasitugas_194/view/DaftarPeserta.kt
+++ app/src/main/java/com/example/questnavigasitugas_194/view/DaftarPeserta.kt
@@ -3,13 +3,20 @@ package com.example.questnavigasitugas_194.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questnavigasitugas_194.R
 
 @OptIn (ExperimentalMaterial3Api::class)
 @Composable
@@ -24,6 +31,20 @@ fun ListPeserta(
             .padding(24.dp),
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {  }
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text (
+                text = stringResource(id = R.string.judul_list),
+                style = MaterialTheme.typography.titleLarge.copy(
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+        }
+    }
 
 }
