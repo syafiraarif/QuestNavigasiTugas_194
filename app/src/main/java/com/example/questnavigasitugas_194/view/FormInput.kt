c7234c90edb27abde2bbca39d1469c9f84c946f9
syairaarif <putrisyafira4444@gmail.com>
2025-11-07 15:56:36 +0700

    menambahkan komponen text untuk judul halaman

--- app/src/main/java/com/example/questnavigasitugas_194/view/FormInput.kt
+++ app/src/main/java/com/example/questnavigasitugas_194/view/FormInput.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,7 +13,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.questnavigasitugas_194.R
 
 
 @OptIn (ExperimentalMaterial3Api::class)
@@ -42,6 +47,11 @@ fun FormInput(
             .padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = stringResource(id = R.string.judul_form),
+            style = MaterialTheme.typography.titleLarge,
+            color = Color.Black
+        )
 
     }
 
