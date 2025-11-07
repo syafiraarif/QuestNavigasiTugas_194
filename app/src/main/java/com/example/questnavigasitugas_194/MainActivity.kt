e03764562c6b717d2a58e7c57edaae1ede08749b
syairaarif <putrisyafira4444@gmail.com>
2025-11-07 13:21:53 +0700

    membuat project navigasi untuk tugas praktikum pertemuan ke - 6

---
+++ app/src/main/java/com/example/questnavigasitugas_194/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.questnavigasitugas_194
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.questnavigasitugas_194.ui.theme.QuestNavigasiTugas_194Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuestNavigasiTugas_194Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuestNavigasiTugas_194Theme {
+        Greeting("Android")
+    }
+}
